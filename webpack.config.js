284a2abcb8ef09c4be747f47af3848b2008ae30d
Askhab Magomedov <asmag86@gmail.com>
2021-07-13 00:48:54 +0300

    Installer html-webpack plugin and done some modifications in webpack-config.js file

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,19 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  
+  mode: 'development',
+  entry: './src/js/index.js',
+  devServer: {
+    contentBase: './build'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development'
+    })
+  ],
+  output: {
+    filename: 'build.js',
+    path: path.resolve(__dirname,dist),
+  },
 };
